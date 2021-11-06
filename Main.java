e44ac35789e4ba12e3bc59d3d5220a32d137dcf1
jason-61 <jasonnk.27@gmail.com>
2021-11-06 15:15:10 +0700

    shibai

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        String text = "Hello World, it's me bicis";
+        String text = "Hello World, it's me shiba army";
         System.out.println(text);
     }
 }

6153b482e3564c2c64accbf64ae65f6de7888551
jason-61 <jasonnk.27@gmail.com>
2021-11-17 12:46:07 +0700

    update conditional
    update logic jika ada tembok maka valuenya ga keubah jd 5(lampu)

--- Individual.java
+++ Individual.java
@@ -49,114 +49,169 @@ public class Individual {
                 if(this.array[i][j] == 0){
                     //JIKA I DAN J BUKAN DI UJUNG
                     if(i != 0 && i != this.array.length - 1 && j != 0 && j != this.array[i].length - 1){
+                        if(this.array[i+1][j] != -5){
                         this.array[i+1][j] = 6;
+                        }
+                        if(this.array[i-1][j] !=-5){
                         this.array[i-1][j] = 6;
+                        }
+                        if(this.array[i][j+1] != -5){
                         this.array[i][j+1] = 6;
+                        }
+                        if(this.array[i][j-1] != -5){
                         this.array[i][j-1] = 6;
+                        }
                     }
 
                     //JIKA I DI UJUNG ATAS DAN J DI TENGAH
                     else if(i == 0 && j != 0 && j != this.array[i].length - 1){
-                        this.array[i+1][j] = 6;
-                        this.array[i][j+1] = 6;
-                        this.array[i][j-1] = 6;
+                        if(this.array[i+1][j] != -5){
+                            this.array[i+1][j] = 6;
+                        }
+                        if(this.array[i][j+1] != -5){
+                            this.array[i][j+1] = 6;
+                        }
+                        if(this.array[i][j-1] != -5){
+                            this.array[i][j-1] = 6;
+                        }
                     }
 
                     //JIKA I DI UJUNG BAWAH DAN J DI TENGAH
                     else if(i == this.array.length -1 && j != 0 && j != this.array[i].length - 1){
-                        this.array[i-1][j] = 6;
-                        this.array[i][j+1] = 6;
-                        this.array[i][j-1] = 6;
+                        if(this.array[i-1][j] !=-5){
+                            this.array[i-1][j] = 6;
+                        }
+                        if(this.array[i][j+1] != -5){
+                            this.array[i][j+1] = 6;
+                        }
+                        if(this.array[i][j-1] != -5){
+                            this.array[i][j-1] = 6;
+                        }
                     }
 
                     //JIKA J DI UJUNG KIRI DAN I DI TENGAH
                     else if(j == 0 && i != 0 && i != this.array.length - 1){
-                        this.array[i+1][j] = 6;
-                        this.array[i-1][j] = 6;
-                        this.array[i][j+1] = 6;
+                        if(this.array[i+1][j] != -5){
+                            this.array[i+1][j] = 6;
+                        }
+                        if(this.array[i-1][j] !=-5){
+                            this.array[i-1][j] = 6;
+                        }
+                        if(this.array[i][j+1] != -5){
+                            this.array[i][j+1] = 6;
+                        }
                     }
 
                     //JIKA J DI UJUNG KANAN DAN I DI TENGAH
                     else if(j == this.array[i].length -1 && i != 0 && i != this.array.length -1){
-                        this.array[i+1][j] = 6;
-                        this.array[i-1][j] = 6;
-                        this.array[i][j-1] = 6;
+                        if(this.array[i+1][j] != -5){
+                            this.array[i+1][j] = 6;
+                        }
+                        if(this.array[i-1][j] !=-5){
+                            this.array[i-1][j] = 6;
+                        }
+                        if(this.array[i][j-1] != -5){
+                            this.array[i][j-1] = 6;
+                        }
                     }
 
                     //JIKA I J DI UJUNG KIRI ATAS
                     else if(j == 0 && i == 0){
-                        this.array[i+1][j] = 6;
-                        this.array[i][j+1] = 6;
+                        if(this.array[i+1][j] != -5){
+                            this.array[i+1][j] = 6;
+                        }
+                        if(this.array[i][j+1] != -5){
+                            this.array[i][j+1] = 6;
+                        }
                     }
 
                     //JIKA I J DI UJUNG KANAN ATAS
                     else if(j == this.array[j].length - 1 && i == 0){
-                        this.array[i+1][j] = 6;
-                        this.array[i][j-1] = 6;
+                        if(this.array[i+1][j] != -5){
+                            this.array[i+1][j] = 6;
+                        }
+                        if(this.array[i][j-1] != -5){
+                             this.array[i][j-1] = 6;
+                        }
                     }
 
                     //JIKA I J DI UJUNG KIRI BAWAH
                     else if(j == 0 && i == this.array[i].length-1){
-                        this.array[i-1][j] = 6;
-                        this.array[i][j+1] = 6;
+                        if(this.array[i-1][j] !=-5){
+                            this.array[i-1][j] = 6;
+                        }
+                        if(this.array[i][j+1] != -5){
+                            this.array[i][j+1] = 6;
+                        }
                     }
 
                     //JIKA I J DI UJUNG KANAN BAWAH
                     else if(j == this.array[j].length -1 && i == this.array[i].length-1){
-                        this.array[i-1][j] = 6;
-                        this.array[i][j-1] = 6;
+                        if(this.array[i-1][j] !=-5){
+                            this.array[i-1][j] = 6;
+                        }
+                        if(this.array[i][j-1] != -5){
+                            this.array[i][j-1] = 6;
+                        }
                     }
 
 
                 }
                 //JIKA KETEMU 4, HARUS ADA LAMPU DI SEKITARNYA
                 else if(this.array[i][j] == 4){
+                    //JIKA I DAN J BUKAN DI UJUNG
                     if(i != 0 && i != this.array.length - 1 && j != 0 && j != this.array[i].length - 1){
-                        this.array[i+1][j] = 5;
-                        this.array[i-1][j] = 5;
-                        this.array[i][j+1] = 5;
-                        this.array[i][j-1] = 5;
+                        if(this.array[i+1][j] == -5 || this.array[i-1][j] == -5 || this.array[i][j+1] == -5 || this.array[i][j-1] == -5){
+                            this.fitness+=100;
+                        }
+                        else {
+                            this.array[i+1][j] = 6;
+                            this.array[i-1][j] = 6;
+                            this.array[i][j+1] = 6;
+                            this.array[i][j-1] = 6;
+                        }
                     }
 
                     //JIKA I DI UJUNG ATAS DAN J DI TENGAH
                     else if(i == 0 && j != 0 && j != this.array[i].length - 1){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA I DI UJUNG BAWAH DAN J DI TENGAH
                     else if(i == this.array.length -1 && j != 0 && j != this.array[i].length - 1){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA J DI UJUNG KIRI DAN I DI TENGAH
                     else if(j == 0 && i != 0 && i != this.array.length - 1){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA J DI UJUNG KANAN DAN I DI TENGAH
                     else if(j == this.array[i].length -1 && i != 0 && i != this.array.length -1){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA I J DI UJUNG KIRI ATAS
                     else if(j == 0 && i == 0){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA I J DI UJUNG KANAN ATAS
                     else if(j == this.array[j].length - 1 && i == 0){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA I J DI UJUNG KIRI BAWAH
                     else if(j == 0 && i == this.array[i].length-1){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
 
                     //JIKA I J DI UJUNG KANAN BAWAH
                     else if(j == this.array[j].length -1 && i == this.array[i].length-1){
-                        this.fitness +=100;
+                        this.fitness  += 100;
                     }
+
                 }
 
             }

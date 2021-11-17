aa6844f173a83f575f69b39ab92ddc2187d3709b
jason-61 <jasonnk.27@gmail.com>
2021-11-17 20:57:10 +0700

    tip of the iceberg

--- LampCombination.java
+++ LampCombination.java
@@ -1,7 +1,27 @@
 public final class LampCombination{
     int num;
-    public LampCombination(int num){
+    int [] [] array;
+    int row;
+    int col;
+    public LampCombination(int num,int[][]array,int row,int col){
         this.num = num;
+        this.array = array;
+    }
+
+    public void above(){
+        this.array[row-1][col] = 5;
+    }
+
+    public void under(){
+        this.array[row+1][col] = 5;
+    }
+
+    public void left (){
+        this.array[row][col-1] = 5;
+    }
+
+    public void right(){
+        this.array[row][col+1] = 5;
     }
 
     

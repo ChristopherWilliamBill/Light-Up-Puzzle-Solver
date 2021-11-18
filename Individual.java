a0c6a7aafb4b72a69000ac79d00f090b028472ee
ChristopherWilliam6181901048 <6181901048@student.unpar.ac.id>
2021-11-18 20:55:01 +0700

    Generate array jawaban

--- Individual.java
+++ Individual.java
@@ -2,8 +2,10 @@ import java.util.Random;
 import java.io.*;
 
 public class Individual {
-    private int[][] array;
+    private int[] arrayNBS;
+    private int[][] arraySoal;
     private int fitness;
+    private Random random;
 
     /*
 
@@ -36,336 +38,158 @@ public class Individual {
          6 : tidak ada lampu   
     */
 
-    public Individual(int[][] array){
+    public Individual(int[] arrayNBS, int[][] arraySoal, Random random){
+        this.random = random;
+        this.arraySoal = arraySoal;
+        this.arrayNBS = arrayNBS;
         this.fitness = 0;
-        this.array = array;
     }
 
-    public void setFitness(){
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array[i].length; j++){
+    private int[][] generateArrayJawaban(){
+        int length = this.arraySoal.length;
+        int[][] jawaban = new int[length][length];
+        int NBSCounter = 0;
 
-                //JIKA KETEMU 0, TIDAK BOLEH ADA LAMPU DI SEKITARNYA
-                if(this.array[i][j] == 0){
-                    //JIKA I DAN J BUKAN DI UJUNG
-                    if(i != 0 && i != this.array.length - 1 && j != 0 && j != this.array[i].length - 1){
-                        if(this.array[i+1][j] != -5){
-                        this.array[i+1][j] = 6;
+        for(int i = 0; i < length; i++){
+            for(int j = 0; j < length; j++){
+                if(arraySoal[i][j] == 0){
+                    NBSCounter++;
+                    try {
+                        jawaban[i][j+1] = 6;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                    try {
+                        jawaban[i][j-1] = 6;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                    try {
+                        jawaban[i+1][j] = 6;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                    try {
+                        jawaban[i-1][j] = 6;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                }else if(arraySoal[i][j] == 4){
+                    NBSCounter++;
+                    try {
+                        jawaban[i][j+1] = 5;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                    try {
+                        jawaban[i][j-1] = 5;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                    try {
+                        jawaban[i+1][j] = 5;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+
+                    try {
+                        jawaban[i-1][j] = 5;
+                    }catch (Exception e){
+                        fitness += 100;
+                    }
+                }else if(arraySoal[i][j] == 1){
+                    NBSCounter++;
+                    if(arrayNBS[NBSCounter] == 1){
+                        try {
+                            jawaban[i-1][j] = 5;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
-                        if(this.array[i-1][j] !=-5){
-                        this.array[i-1][j] = 6;
-                        }
-                        if(this.array[i][j+1] != -5){
-                        this.array[i][j+1] = 6;
-                        }
-                        if(this.array[i][j-1] != -5){
-                        this.array[i][j-1] = 6;
+                        try {
+                            jawaban[i+1][j] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j-1] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j+1] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
                     }
 
-                    //JIKA I DI UJUNG ATAS DAN J DI TENGAH
-                    else if(i == 0 && j != 0 && j != this.array[i].length - 1){
-                        if(this.array[i+1][j] != -5){
-                            this.array[i+1][j] = 6;
+                    if(arrayNBS[NBSCounter] == 2){
+                        try {
+                            jawaban[i-1][j] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
-                        if(this.array[i][j+1] != -5){
-                            this.array[i][j+1] = 6;
-                        }
-                        if(this.array[i][j-1] != -5){
-                            this.array[i][j-1] = 6;
-                        }
-                    }
-
-                    //JIKA I DI UJUNG BAWAH DAN J DI TENGAH
-                    else if(i == this.array.length -1 && j != 0 && j != this.array[i].length - 1){
-                        if(this.array[i-1][j] !=-5){
-                            this.array[i-1][j] = 6;
-                        }
-                        if(this.array[i][j+1] != -5){
-                            this.array[i][j+1] = 6;
-                        }
-                        if(this.array[i][j-1] != -5){
-                            this.array[i][j-1] = 6;
+                        try {
+                            jawaban[i+1][j] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j-1] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j+1] = 5;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
                     }
 
-                    //JIKA J DI UJUNG KIRI DAN I DI TENGAH
-                    else if(j == 0 && i != 0 && i != this.array.length - 1){
-                        if(this.array[i+1][j] != -5){
-                            this.array[i+1][j] = 6;
+                    if(arrayNBS[NBSCounter] == 3){
+                        try {
+                            jawaban[i-1][j] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
-                        if(this.array[i-1][j] !=-5){
-                            this.array[i-1][j] = 6;
-                        }
-                        if(this.array[i][j+1] != -5){
-                            this.array[i][j+1] = 6;
-                        }
-                    }
-
-                    //JIKA J DI UJUNG KANAN DAN I DI TENGAH
-                    else if(j == this.array[i].length -1 && i != 0 && i != this.array.length -1){
-                        if(this.array[i+1][j] != -5){
-                            this.array[i+1][j] = 6;
-                        }
-                        if(this.array[i-1][j] !=-5){
-                            this.array[i-1][j] = 6;
-                        }
-                        if(this.array[i][j-1] != -5){
-                            this.array[i][j-1] = 6;
+                        try {
+                            jawaban[i+1][j] = 5;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j-1] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j+1] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
                     }
 
-                    //JIKA I J DI UJUNG KIRI ATAS
-                    else if(j == 0 && i == 0){
-                        if(this.array[i+1][j] != -5){
-                            this.array[i+1][j] = 6;
+                    if(arrayNBS[NBSCounter] == 4){
+                        try {
+                            jawaban[i-1][j] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
-                        if(this.array[i][j+1] != -5){
-                            this.array[i][j+1] = 6;
-                        }
-                    }
-
-                    //JIKA I J DI UJUNG KANAN ATAS
-                    else if(j == this.array[j].length - 1 && i == 0){
-                        if(this.array[i+1][j] != -5){
-                            this.array[i+1][j] = 6;
-                        }
-                        if(this.array[i][j-1] != -5){
-                             this.array[i][j-1] = 6;
-                        }
-                    }
-
-                    //JIKA I J DI UJUNG KIRI BAWAH
-                    else if(j == 0 && i == this.array[i].length-1){
-                        if(this.array[i-1][j] !=-5){
-                            this.array[i-1][j] = 6;
-                        }
-                        if(this.array[i][j+1] != -5){
-                            this.array[i][j+1] = 6;
+                        try {
+                            jawaban[i+1][j] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j-1] = 5;
+                        }catch (Exception e){
+                            fitness += 100;
+                        }try {
+                            jawaban[i][j+1] = 6;
+                        }catch (Exception e){
+                            fitness += 100;
                         }
                     }
-
-                    //JIKA I J DI UJUNG KANAN BAWAH
-                    else if(j == this.array[j].length -1 && i == this.array[i].length-1){
-                        if(this.array[i-1][j] !=-5){
-                            this.array[i-1][j] = 6;
-                        }
-                        if(this.array[i][j-1] != -5){
-                            this.array[i][j-1] = 6;
-                        }
-                    }
-                }
-
-                else if (this.array[i][j] == 1){
-                    int random = (int)(Math.random() * 3 + 0);
-                    int top = this.array[i-1][j];
-                    int bottom = this.array[i+1][j];
-                    int left = this.array[i][j-1];
-                    int right = this.array[i][j+1];
-                    if(i != 0 && i != this.array.length - 1 && j != 0 && j != this.array[i].length - 1){
-                        while(top != 5 || bottom != 5 || left != 5 || right != 5){
-                            if(random == 0){
-                                if(top != -5){
-                                    top = 6;
-                                }
-                            }
-                            else if(random == 1){
-                                if(left != -5){
-                                    left = 5;
-                                }
-                            }
-                            else if(random == 2){
-                                if(bottom != -5){
-                                    bottom = 5;
-                                }
-                            }
-                            else if(random == 3){
-                                if( right != -5){
-                                    right = 5;
-                                }
-                            }
-                            random = (int)(Math.random() * 3 + 0);
-                        }
-                    }
-                }
-
-                else if (this.array[i][j] == 2){
-                    
-                }
-
-                else if (this.array[i][j] == 3){
-
-                }
-
-                
-                //JIKA KETEMU 4, HARUS ADA LAMPU DI SEKITARNYA
-                else if(this.array[i][j] == 4){
-                    //JIKA I DAN J BUKAN DI UJUNG
-                    if(i != 0 && i != this.array.length - 1 && j != 0 && j != this.array[i].length - 1){
-                        if(this.array[i+1][j] == -5 || this.array[i-1][j] == -5 || this.array[i][j+1] == -5 || this.array[i][j-1] == -5){
-                            this.fitness+=100;
-                        }
-                        else {
-                            this.array[i+1][j] = 6;
-                            this.array[i-1][j] = 6;
-                            this.array[i][j+1] = 6;
-                            this.array[i][j-1] = 6;
-                        }
-                    }
-
-                    //JIKA I DI UJUNG ATAS DAN J DI TENGAH
-                    else if(i == 0 && j != 0 && j != this.array[i].length - 1){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA I DI UJUNG BAWAH DAN J DI TENGAH
-                    else if(i == this.array.length -1 && j != 0 && j != this.array[i].length - 1){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA J DI UJUNG KIRI DAN I DI TENGAH
-                    else if(j == 0 && i != 0 && i != this.array.length - 1){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA J DI UJUNG KANAN DAN I DI TENGAH
-                    else if(j == this.array[i].length -1 && i != 0 && i != this.array.length -1){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA I J DI UJUNG KIRI ATAS
-                    else if(j == 0 && i == 0){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA I J DI UJUNG KANAN ATAS
-                    else if(j == this.array[j].length - 1 && i == 0){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA I J DI UJUNG KIRI BAWAH
-                    else if(j == 0 && i == this.array[i].length-1){
-                        this.fitness  += 100;
-                    }
-
-                    //JIKA I J DI UJUNG KANAN BAWAH
-                    else if(j == this.array[j].length -1 && i == this.array[i].length-1){
-                        this.fitness  += 100;
-                    }
-
-                }
-
-            }
-        } 
-    }
-
-    public static int[][] importFile(String fileName) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        int[][] numArray;
-        String line = reader.readLine();
-        if (line == null) {
-            throw new IllegalArgumentException("There was no 1st line.");
-        }
-        String[] dimensions = line.split("\\s+");
-        try {
-            int rows = Integer.parseInt(dimensions[0]);
-            int cols = Integer.parseInt(dimensions[1]);
-            // TODO: check for negative values.
-            numArray = new int[rows][cols];
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("First line of file has to be 'rows cols'");
-        }
-    
-        int row = 0; 
-    
-        while ((line = reader.readLine()) != null && row < numArray.length) {
-            String[] tokens = line.split("\\s+");
-            if (tokens.length > numArray[row].length) {
-                throw new IllegalArgumentException("Too many values provided in matrix row " + row);
-            }
-            // to less values will be filled with 0. If you don't want that
-            // you have to uncomment the next 3 lines.
-            //if (tokens.length < numArray[row].length) {
-            //  throw new IllegalArgumentException("Not enough values provided in matrix row " + row);
-            //}
-            for(int column = 0; column < tokens.length; column++) {
-                try {
-                    int value = Integer.parseInt(tokens[column]);
-                    numArray[row][column] = value; 
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("Non numeric value found in matrix row " + row + ", column " + column);
                 }
             }
-            row++;
         }
-        if (line != null) {
-            // there were too many rows provided.
-            // Superflous ones are ignored.
-            // You can also throw exception, if you want.
-        }
-        if (row < numArray.length) {
-            // There were too less rows in the file. If that's OK, the
-            // missing rows will be interpreted as all 0.
-            // If that's OK with you, you can comment out this whole
-            // if block
-            throw new IllegalArgumentException("Expected " + numArray.length + " rows, there only were " + row);
-        }
-        try {
-            reader.close(); // never forget to close a stream.
-        } catch (IOException e) { }
-        return numArray;
-    }
-
-    public Individual doCrossover(Individual other){
-        //2 array akan dibagi 2 dan digabungkan pada titik pembagian
-
-        int length = this.array.length;
-
-        //titik pembagian adalah panjang array dibagi 2
-        int crossPoint = Math.ceil(length/ 2);
-
-        Individual child = new Individual(new int[length][length]);
-
-        for(int i = 0; i < crossPoint; i++){
-            for(int j = 0; j < this.array[i].length; j++){
-                child.array[i][j] = this.array[i][j];
-            }
-        }
-
-        for(int i = crossPoint; i < length; i++){
-            for(int j = 0; j < this.array[i].length; j++){
-                child.array[i][j] = other.array[i][j];
-            }
-        }
-
-        return child;
-    }
-
-    public void doMutation(){
-        
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        Random r = new Random();
-
-        int low = 4;
-        int high = 6;
-
-        // int temp[][] = new int[7][7];
-        // for(int i = 0; i < 7; i++){
-        //     for(int j = 0; j < 7; j++){
-        //         temp[i][j] = r.nextInt(high - low) + low;
-        //     }
-        // }
-        
-        int temp[][] = importFile("Array.txt");
-        Individual i = new Individual(temp);
-
-        i.setFitness();
-
-        System.out.println(i.fitness);
-        System.out.println();
-
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,100 @@
+import java.util.Random;
+import java.io.*;
+import java.util.LinkedList;
+
 public class Main{
-    public static void main(String[] args) {
-        String text = "Hello World, it's me shiba army";
-        System.out.println(text);
+
+    //method untuk mengimport soal dari file.txt
+    public static int[][] importFile(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        int[][] numArray;
+        String line = reader.readLine();
+        if (line == null) {
+            throw new IllegalArgumentException("There was no 1st line.");
+        }
+        String[] dimensions = line.split("\\s+");
+        try {
+            int rows = Integer.parseInt(dimensions[0]);
+            int cols = Integer.parseInt(dimensions[1]);
+            // TODO: check for negative values.
+            numArray = new int[rows][cols];
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("First line of file has to be 'rows cols'");
+        }
+    
+        int row = 0; 
+    
+        while ((line = reader.readLine()) != null && row < numArray.length) {
+            String[] tokens = line.split("\\s+");
+            if (tokens.length > numArray[row].length) {
+                throw new IllegalArgumentException("Too many values provided in matrix row " + row);
+            }
+            // to less values will be filled with 0. If you don't want that
+            // you have to uncomment the next 3 lines.
+            //if (tokens.length < numArray[row].length) {
+            //  throw new IllegalArgumentException("Not enough values provided in matrix row " + row);
+            //}
+            for(int column = 0; column < tokens.length; column++) {
+                try {
+                    int value = Integer.parseInt(tokens[column]);
+                    numArray[row][column] = value; 
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Non numeric value found in matrix row " + row + ", column " + column);
+                }
+            }
+            row++;
+        }
+        if (line != null) {
+            // there were too many rows provided.
+            // Superflous ones are ignored.
+            // You can also throw exception, if you want.
+        }
+        if (row < numArray.length) {
+            // There were too less rows in the file. If that's OK, the
+            // missing rows will be interpreted as all 0.
+            // If that's OK with you, you can comment out this whole
+            // if block
+            throw new IllegalArgumentException("Expected " + numArray.length + " rows, there only were " + row);
+        }
+        try {
+            reader.close(); // never forget to close a stream.
+        } catch (IOException e) { }
+        return numArray;
+    }
+    public static void main(String[] args) throws IOException {
+
+        long seed = 123456789;
+
+        Random random = new Random(seed);
+
+        int soal[][] = importFile("ArraySoal.txt");
+
+        int NumberOfBlackSquares = 0;
+
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+        for(int i = 0; i < soal.length; i++){
+            for(int j = 0; j < soal[i].length; j++){
+                if(soal[i][j] > -1){
+                    NumberOfBlackSquares++;
+                    linkedList.add(soal[i][j]);
+                }
+            }
+        }
+
+        for(int i = 0; i < 10; i++){
+            int[] NBS = new int[NumberOfBlackSquares];
+            for(int j = 0; j < NumberOfBlackSquares; j++){
+                int temp = linkedList.removeFirst();
+                if(temp == 0 || temp == 4){
+                    NBS[j] = 1;
+                }else if(temp == 1 || temp == 3){
+                    NBS[j] = random.nextInt(5 - 1) + 1;
+                }else if(temp == 2){
+                    NBS[j] = random.nextInt(7 - 1) + 1;
+                }
+            }
+            Individual individual = new Individual(NBS, random);
+        }
     }
 }
